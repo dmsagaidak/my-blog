1ab7eed60b08161a9fe9be25425296b178915853
Dmitry Sagaidak <dm.sagaidak@gmail.com>
2022-12-01 12:10:19 +0600

    Fixed bugs in Add.tsx

--- src/containers/Add/Add.tsx
+++ src/containers/Add/Add.tsx
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
+import React from 'react';
 import PostForm from '../../components/PostForm/PostForm';
-import {Post, PostApi} from "../../types";
+import {PostApi} from "../../types";
 import axiosApi from "../../axiosApi";
 import {useNavigate} from "react-router-dom";
 
@@ -9,7 +9,7 @@ const Add: React.FC = () => {
   const navigate = useNavigate();
   const addPost = async (post: PostApi) => {
     try{
-      await axiosApi.post ('/posts.json' + post);
+      await axiosApi.post ('/posts.json', post);
       navigate ('/')
     }finally{
 
