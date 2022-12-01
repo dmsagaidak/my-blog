72af3e9cb92bd47378cd1b6f80c610d75b8f614b
Dmitry Sagaidak <dm.sagaidak@gmail.com>
2022-12-01 12:29:41 +0600

    Added function removePost, the function was attached to Remove button in PostItem.tsx

--- src/components/Posts/PostItem.tsx
+++ src/components/Posts/PostItem.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import {Post} from "../../types";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import axiosApi from "../../axiosApi";
 
 interface Props {
   post: Post
 }
 
 const PostItem: React.FC<Props> = ({post}) => {
+  const navigate = useNavigate()
+
+  const removePost = async (post: Post) => {
+    try{
+      await axiosApi.delete('/posts/' + post.id + '.json');
+      navigate('/');
+    }finally{
+
+    }
+  }
+
+
   return (
     <div className="card mb-2 p-1">
       <p className="card-title m-0"><strong>{post.title}</strong></p>
@@ -14,7 +27,7 @@ const PostItem: React.FC<Props> = ({post}) => {
       <div>
         <button className="btn btn-sm btn-primary">See More</button>
         <Link to={"edit-post/" + post.id} className="btn btn-sm btn-secondary">Edit</Link>
-        <button className="btn btn-sm btn-danger">Remove</button>
+        <button className="btn btn-sm btn-danger" onClick={() => removePost(post)}>Remove</button>
       </div>
       <hr/>
       <p className="card-text small">{post.message}</p>
